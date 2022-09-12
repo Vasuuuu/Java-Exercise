ce427bbd2d5f66422db69a428365dc58b3e66789
Vasu <87427261+Vasuuuu@users.noreply.github.com>
2022-09-12 15:35:41 +0530

    Update Avg_of_3_nums.java

--- Avg_of_3_nums.java
+++ Avg_of_3_nums.java
@@ -6,7 +6,11 @@ public class Avg_of_3_nums{
         int n1=in.nextInt();
         int n2=in.nextInt();
         int n3=in.nextInt();
-        int avg = (n1+n2+n3)/3;
-        System.out.println("Average of "+n1+", "+n2+", "+n3+" : "+avg);
+        avg_3_nums(n1,n2,n3);
+    }
+    static void avg_3_nums(int n1,int n2,int n3){
+    int avg = (n1+n2+n3)/3;
+    System.out.println("Average of "+n1+", "+n2+", "+n3+" : "+avg);
     }
 }
+
